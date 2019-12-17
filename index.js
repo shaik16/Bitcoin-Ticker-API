01f50d9155a580c1701b8b4bd7a78b3de0550eee
Shaik Sadath <sadath.sa@gmail.com>
2019-12-17 22:06:54 +0530

    0.0.2

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ app.get("/",function(req,res){
 })
 
 app.post("/",function(req,res){
-    console.log(req.body.crypto)
-})
-request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD",function(error,response,body){
-    console.log(body);
-    
+    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD",function(error,response,body){
+        var data=JSON.parse(body);
+        var price=data.last;
+        res.send("<h1>the current price of btc is "+price+" USD</h1>") 
+    })
 })
